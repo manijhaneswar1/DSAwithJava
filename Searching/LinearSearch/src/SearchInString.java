c318f33244c81a020e7c891a3ac717306ccca55a
Mani Jhaneswar <manijhaneswar@gmail.com>
2024-12-08 14:40:55 +0530

    SearchInString

---
+++ Searching/LinearSearch/src/SearchInString.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.ArrayList;
+
+public class SearchInString {
+    public static void main(String[] args) {
+        String name="mani";
+        char target='i';
+        System.out.println( search(name,target));
+    }
+    static boolean search(String str,char target){
+        if(str.isEmpty()){
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
