47aa5e4a54668febe6dcd4b151374e74bb1e1854
Mani Jhaneswar <manijhaneswar@gmail.com>
2024-12-09 12:19:58 +0530

    StockBuySell

---
+++ Arrays/src/StockBuySell.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class StockBuySell {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of Arr : ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Profit(arr));
+    }
+
+    static int Profit(int[] arr){
+        int minPrice=arr[0];
+        for (int j : arr) {
+            if (j < minPrice) {
+                minPrice = j;
+            }
+        }
+        int priceIndex=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==minPrice){
+                priceIndex=i;
+            }
+        }
+        int max=minPrice;
+        for (int i = priceIndex; i < arr.length; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        return max-minPrice;
+    }
+}
