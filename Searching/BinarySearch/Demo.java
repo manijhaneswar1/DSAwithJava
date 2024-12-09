402b8f7a93748c1a1912f876c131f6ea35b6736e
Mani Jhaneswar <manijhaneswar@gmail.com>
2024-12-09 15:20:36 +0530

    BinaryDemoCode

--- Searching/BinarySearch/Demo.java
+++ Searching/BinarySearch/Demo.java
@@ -1,5 +1,42 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hyy Binary");
+        int[] arr={1,2,3,4,5};
+        int target=1;
+        int[] arr2={5,4,3,2,1};
+        int target2=1;
+        System.out.println("index of Given target in Arr {Ascending order}: " + BinaryAss(arr,target));
+        System.out.println("index of Given target in Arr {Descending order} : " + BinaryDec(arr2,target2));
+    }
+    static int BinaryAss(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        while (start <=end) {
+            int mid=start + (end-start)/2;
+            if(target>arr[mid]){
+                start=mid+1;
+            } else if (target < arr[mid]) {
+                end=mid-1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    static int BinaryDec(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        while (start <=end) {
+            int mid=start + (end-start)/2;
+            if(target>arr[mid]){
+                end=mid-1;
+            } else if (target < arr[mid]) {
+                start=mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
