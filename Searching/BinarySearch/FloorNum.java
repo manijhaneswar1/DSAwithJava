492a5d5c625d58f3e65ecca2f623a940b9bd324e
Mani Jhaneswar <manijhaneswar@gmail.com>
2024-12-10 10:28:47 +0530

    FloorNum

--- Searching/BinarySearch/FloorNum.java
+++ Searching/BinarySearch/FloorNum.java
@@ -1,14 +1,14 @@
-public class CeilNum {
+public class FloorNum {
     public static void main(String[] args) {
         int[] arr = {1, 5, 6, 7, 10, 12};
-        int target = 11;
-        System.out.println(ceilFun(arr, target));
+        int target = 1000;
+        System.out.println(floorFun(arr, target));
     }
-    static int ceilFun(int[] arr, int target) {
-        int ceil = -1;
+    static int floorFun(int[] arr, int target) {
+        int floor = -1;
         int start = 0;
         int end = arr.length - 1;
-        if (target > arr[end]) {
+        if (target < arr[start]) {
             return -1;
         }
         while (start <= end) {
@@ -16,12 +16,12 @@ public class CeilNum {
             if (arr[mid] == target) {
                 return arr[mid];
             } else if (arr[mid] < target) {
+                floor = arr[mid];
                 start = mid + 1;
             } else {
-                ceil=arr[mid];
                 end = mid - 1;
             }
         }
-        return ceil;
+        return floor;
     }
 }
