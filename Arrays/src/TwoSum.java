c7bc8ce0c68680615b8e9f8438d31bf2d46797a2
Mani Jhaneswar <manijhaneswar@gmail.com>
2024-12-10 20:42:20 +0530

    TwoSum

---
+++ Arrays/src/TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr={2,7,11,5};
+        int target=199;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+    static int[] twoSum(int[] arr,int target){
+       int[] ans=new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            int num=arr[i];
+            int secNum=target-num;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j]==secNum){
+                    ans[0]=i;
+                    ans[1]=j;
+                    return ans;
+                }
+            }
+        }
+       return new int[0];
+    }
+}
