6ae3140551ea6d34617e0402684db4734a7aefb1
manijhaneswar1 <manijhaneswar@gmail.com>
2024-12-11 21:20:22 +0530

    PeakIndexMountain

---
+++ Searching/BinarySearch/PeakIndexMountain.java
@@ -0,0 +1,19 @@
+public class PeakIndexMountain {
+    public static void main(String[] args) {
+        int[] arr={1,2};
+        System.out.println(findPeakElement(arr));
+    }
+    static int findPeakElement(int[] arr) {
+        int s=0;
+        int e=arr.length-1;
+        while(s < e){
+            int mid=s + (e-s)/2;
+            if( arr[mid] > arr[mid+1]){
+                e=mid;
+            }else {
+                s=mid+1;
+            }
+        }
+        return s;
+    }
+}
