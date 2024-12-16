1c20a86084575f5e590f2b67441ada7d24c146e6
manijhaneswar1 <manijhaneswar@gmail.com>
2024-12-16 11:08:48 +0530

    BubbleSortDemo

--- Sorting/BubbleSort/Demo.java
+++ Sorting/BubbleSort/Demo.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hy Bubble sort");
+        int[] arr={1,2,3,4,5};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubbleSort(int[] nums){
+        boolean swapped;
+        int n=nums.length;
+        for (int i = 0; i < n; i++) {
+            swapped=false;
+            for (int j = 0; j < n-i-1; j++) {
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j+1];
+                    nums[j+1]=nums[j];
+                    nums[j]=temp;
+                    swapped=true;
+                }
+            }
+            if(swapped==false){
+                break;
+            }
+        }
     }
 }
