d917d747fcf3c8d4c0f9abc6932b69a3c84b17ad
manijhaneswar1 <manijhaneswar@gmail.com>
2024-12-18 11:43:18 +0530

    MedianOfTwoArrs

---
+++ Sorting/BubbleSort/MedianOfTwoArrs.java
@@ -0,0 +1,25 @@
+public class MedianOfTwoArrs {
+    public static void main(String[] args) {
+        int[] nums1={1,2};
+        int[] nums2={3,4};
+        System.out.println(median(nums1,nums2));
+    }
+    static double median(int[] nums1,int[] nums2){
+        int m=nums1.length;
+        int n=nums2.length;
+        int[] merged=new int[m+n];
+        for (int i = 0; i < m; i++) {
+            merged[i]=nums1[i];
+        }
+        for (int i = 0; i < n; i++) {
+            merged[i+m]=nums2[i];
+        }
+        java.util.Arrays.sort(merged);
+        int l=merged.length;
+        if(l%2==0){
+            return (merged[l/2]+merged[(l/2)-1])/2.0;
+        }else{
+            return merged[l/2];
+        }
+    }
+}
