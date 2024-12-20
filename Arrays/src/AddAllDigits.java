9204ad8f8ffc7fef94a975679bcab68ab3027cbf
manijhaneswar1 <manijhaneswar@gmail.com>
2024-12-20 08:40:03 +0530

    AddAllDigits

---
+++ Arrays/src/AddAllDigits.java
@@ -0,0 +1,23 @@
+public class AddAllDigits {
+    public static void main(String[] args) {
+        int num=11;
+        System.out.println(addDigits(num));
+    }
+    static int addDigits(int num) {
+        if(String.valueOf(num).length()==1){
+            return num;
+        }
+        int sum=0;
+        while(sum>=0){
+            sum+=num%10;
+            sum+=num/10;
+            if(String.valueOf(sum).length()==1){
+                return sum;
+            }else{
+                num=sum;
+                sum=0;
+            }
+        }
+        return -1;
+    }
+}
