ad90b424b8d2fd754db56875601d3887cc667c77
manijhaneswar1 <manijhaneswar@gmail.com>
2024-12-20 10:10:07 +0530

    SelectionDemo

--- Sorting/SelectionSort/Demo.java
+++ Sorting/SelectionSort/Demo.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hyy");
+        int[] arr={0,2,-3,12,100};
+        select(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void select(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int last=arr.length-i-1;
+            int maxIndex=getMaxIndex(arr,0,last);
+            swap(arr,last,maxIndex);
+        }
+    }
+    //ikada finding maxNumber index
+     static int getMaxIndex(int[] arr, int start, int end) {
+        int max=start;
+        for (int i = start; i <= end; i++) {
+            if(arr[i]>arr[max]){
+                max=i;
+            }
+        }
+        return max;
+    }
+    //ikada swaping with last index and max Number index
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
     }
 }
