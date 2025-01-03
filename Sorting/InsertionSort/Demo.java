21b83d774383cf4ec700b60be708c6dda0abde0e
manijhaneswar1 <manijhaneswar@gmail.com>
2025-01-03 10:15:32 +0530

    InsertionSortDemo

--- Sorting/InsertionSort/Demo.java
+++ Sorting/InsertionSort/Demo.java
@@ -2,17 +2,17 @@ import java.util.Arrays;
 
 public class Demo {
     public static void main(String[] args) {
-        int[] arr={3,2,4,1,5};
+        int[] arr={5,3,4,1,2};
         insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void insertionSort(int[] arr){
         int n = arr.length;
-        for (int i = 0; i < n-2; i++) {
+        for (int i = 0; i < n-1; i++) {
             for (int j = i+1; j > 0; j--) {
-                if(arr[j+1]<arr[j]){
-                    int temp=arr[j+1];
-                    arr[j+1]=arr[j];
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
                     arr[j]=temp;
                 }else{
                     break;
