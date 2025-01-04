03214febf7d7101107b20bd83c8a239b9485ae9b
manijhaneswar1 <manijhaneswar@gmail.com>
2025-01-04 15:21:39 +0530

    DuplicateNum

--- Sorting/CyclicSort/DuplicateNum.java
+++ Sorting/CyclicSort/DuplicateNum.java
@@ -1,6 +1,6 @@
 public class DuplicateNum {
     public static void main(String[] args) {
-        int[] nums={3,1,3,4,2};
+        int[] nums={4,3,2,7,8,2,5,6,1};
         System.out.println(duplicate(nums));
     }
     static int duplicate(int[] nums){
