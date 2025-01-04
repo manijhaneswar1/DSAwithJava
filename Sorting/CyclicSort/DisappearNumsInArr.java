384f1bd8275f86b7b9226d7db5d569d8da107f78
manijhaneswar1 <manijhaneswar@gmail.com>
2025-01-04 10:18:08 +0530

    Disappear Nums in Array

--- Sorting/CyclicSort/DisappearNumsInArr.java
+++ Sorting/CyclicSort/DisappearNumsInArr.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class DisappearNumsInArr {
     public static void main(String[] args) {
-        int[] arr = {4,5,3,2,2,3,7,8,6};
+        int[] arr = {1,2,2,3,4,6};
         System.out.println("Missing Numbers in Given " + Arrays.toString(arr) + " :- \n" +findDisappearedNumbers(arr));
     }
 
