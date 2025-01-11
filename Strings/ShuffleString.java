ce685946ac20cb09fcb9959c2dc6d6fdb6412043
manijhaneswar1 <manijhaneswar@gmail.com>
2025-01-11 10:55:45 +0530

    shuffle String

---
+++ Strings/ShuffleString.java
@@ -0,0 +1,14 @@
+public class ShuffleString {
+    public static void main(String[] args) {
+        int[] inidices={4,5,6,0,1,2,3};
+        String s="tajRuhi";
+        System.out.println(restoreString(s,inidices));
+    }
+    static String restoreString(String s, int[] indices) {
+        char[] ans=new char[s.length()];
+        for(int i=0;i<indices.length;i++){
+            ans[indices[i]]=s.charAt(i);
+        }
+        return String.valueOf(ans);
+    }
+}
