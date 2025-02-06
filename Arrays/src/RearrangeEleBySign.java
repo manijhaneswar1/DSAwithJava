0bd9774d383d84eb2efd4854a1a152f5e4684461
manijhaneswar1 <manijhaneswar@gmail.com>
2025-02-06 10:49:09 +0530

    leetcode_2149

--- Arrays/src/RearrangeEleBySign.java
+++ Arrays/src/RearrangeEleBySign.java
@@ -2,25 +2,25 @@ import java.util.Arrays;
 
 public class RearrangeEleBySign {
     public static void main(String[] args) {
-        int[] arr={3,1,-2,-5,2,-4};
+        int[] arr={-1,1};
         System.out.println(Arrays.toString(rearrangeArray(arr)));
     }
     static int[] rearrangeArray(int[] nums) {
-        if(nums.length==0){
-            return new int[0];
-        }
-        int[] res=new int[nums.length];
-        int positiveIndex=0;
-        int negativeIndex=1;
+        if(nums.length==0) return new int[0];
+
+        int[] resArr = new int[nums.length];
+        int pIdx = 0;
+        int nIdx = 1;
+
         for(int num : nums){
-            if(num>0 && positiveIndex < nums.length){
-                res[positiveIndex]=num;
-                positiveIndex+=2;
-            }else{
-                res[negativeIndex]=num;
-                negativeIndex+=2;
+            if(num > 0 && pIdx < nums.length){
+                resArr[pIdx]=num;
+                pIdx = pIdx + 2;
+            }else if(num < 0 && nIdx < nums.length){
+                resArr[nIdx]=num;
+                nIdx = nIdx+2;
             }
         }
-        return res;
+        return resArr;
     }
 }
