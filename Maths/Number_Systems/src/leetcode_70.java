8f0af20458fdf1b12669e8b7d3e70761e9e607c8
mani jhaneswar <manijhaneswar@gmail.com>
2025-03-14 13:17:19 +0530

    leetCode_70

---
+++ Maths/Number_Systems/src/leetcode_70.java
@@ -0,0 +1,17 @@
+public class leetcode_70 {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(3));
+    }
+    static int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
+    }
+}
